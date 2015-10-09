8244b36c9967b68cfb371dcc3e4ff5a1b8e8c5fe
Silvan Heller <silvanheller@me.com>
2015-10-09 15:52:01 +0200

    a

--- Project/src/ch/unibas/informatik/hs15/cs203/datarepository/processing/DataRepositoryImpl.java
+++ Project/src/ch/unibas/informatik/hs15/cs203/datarepository/processing/DataRepositoryImpl.java
@@ -95,6 +95,7 @@ class DataRepositoryImpl implements DataRepository {
 	 */
 	private void move(Path source, Path target) throws IOException {
 		Path newTarget = Paths.get(target.toString(), source.getFileName().toString());
+		source.toFile().renameTo(newTarget.toFile());
 	}
 
 	/**
