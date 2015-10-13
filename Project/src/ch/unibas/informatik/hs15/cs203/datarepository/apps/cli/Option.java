84e3f59d1a385065b464bf824b613b10151370d1
sauterl <loris.sauter@gmail.com>
2015-10-13 14:04:49 +0200

    Javadoc Update

--- Project/src/ch/unibas/informatik/hs15/cs203/datarepository/apps/cli/Option.java
+++ Project/src/ch/unibas/informatik/hs15/cs203/datarepository/apps/cli/Option.java
@@ -1,5 +1,25 @@
 package ch.unibas.informatik.hs15.cs203.datarepository.apps.cli;
 
+/**
+ * The {@link Option} enum represents available options for commands of this application.
+ * 
+ * Options have an {@link Option#OPTION_MARKER} which is an indicator that a given argument is an option name.
+ * Without this marker, options are not recognized as themselves.
+ * 
+ * Usually options do have an argument, which is directly followed by the option name, but some
+ * options do not have an argument and are called 'flag' options.
+ * 
+ * An example of an option with argument would be <code>--description "The description"</code>
+ * Do note the quotes of the text and the whitespace between the option's name (description) and its
+ * argument ("The description").
+ * 
+ * The option <code>--verbose</code> is a typical 'flag' option.
+ * 
+ * <b>Note:</b> Option names are case insensitive
+ * 
+ * @author Loris
+ *
+ */
 public enum Option{
     
     DESCRIPTION,
