1055668be47c439834fd253302829de0aa77526a
sauterl <loris.sauter@gmail.com>
2015-10-21 10:02:50 +0200

    MDS update: get(Criteria) written

--- Project/src/ch/unibas/informatik/hs15/cs203/datarepository/processing/MetaDataStorage.java
+++ Project/src/ch/unibas/informatik/hs15/cs203/datarepository/processing/MetaDataStorage.java
@@ -1,11 +1,14 @@
 package ch.unibas.informatik.hs15.cs203.datarepository.processing;
 
+import java.util.Collection;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.TreeMap;
 import java.util.Vector;
 
+import ch.unibas.informatik.hs15.cs203.datarepository.api.Criteria;
 import ch.unibas.informatik.hs15.cs203.datarepository.api.MetaData;
 
 /**
@@ -28,6 +31,54 @@ class MetaDataStorage {
 		if (entries != null && entries.length > 0) {
 			initMap(entries);
 		}
+	}
+
+	public List<MetaData> get(final Criteria criteria) {
+		if(criteria == null){
+			throw new IllegalArgumentException("Criteria is null");
+		}
+		Vector<MetaData> out = new Vector<MetaData>();
+		if(criteria.getId() != null){
+			out.add(get(criteria.getId() ));
+			return out;
+		}
+		Vector<List<String>> matching = new Vector<List<String>>();
+		if(criteria.getAfter() != null){
+			matching.add(findAfter(criteria.getAfter() ));
+		}
+		if(criteria.getBefore() != null){
+			matching.add(findBefore(criteria.getBefore() ));
+		}
+		if(criteria.getName() != null){
+			matching.add(findForName(criteria.getName() ));
+		}
+		if(criteria.getText() != null){
+			matching.add(findTextContains(criteria.getText() ));
+		}
+		Vector<String> ids = new Vector<String>();
+		for(List<String> l : matching){
+			//if l is first element: addAll to ids, otherwise only intersection
+			if(ids.size() > 0){
+				ids.retainAll(l);
+			}else{
+				ids.addAll(l);
+			}
+		}
+		return getAll(ids);
+	}
+
+	/**
+	 * Returns the {@link MetaData} with specified ID. If no meta data with such
+	 * an ID was found, <tt>null</tt> is returned.
+	 * 
+	 * @param id
+	 *            The ID of the meta data to get.
+	 * @return The found meta data with specified ID or <tt>null</tt> if none
+	 *         exists with such an ID.
+	 * @see TreeMap#get(Object)
+	 */
+	public MetaData get(final String id) {
+		return idMap.get(id);
 	}
 
 	/**
@@ -61,51 +112,91 @@ class MetaDataStorage {
 		return idRes && timeRes;
 	}
 	
-	/**
-	 * Returns the {@link MetaData} with specified ID.
-	 * If no meta data with such an ID was found, <tt>null</tt> is returned.
-	 * @param id The ID of the meta data to get.
-	 * @return The found meta data with specified ID or <tt>null</tt> if none exists with such an ID.
-	 * @see TreeMap#get(Object)
-	 */
-	public MetaData get(String id){
-		return idMap.get(id);
+	public MetaData replace(final MetaData meta){
+		if(meta == null){
+			throw new IllegalArgumentException("Cannot replace meta data null.");
+		}
+		if(!idMap.containsKey(meta.getId() )){
+			throw new IllegalArgumentException("Cannot replace non existing meta data");
+		}
+		return idMap.put(meta.getId(), meta);
 	}
-	
-	/**
-	 * Returns a {@link List} of {@link MetaData}s for the given name.
-	 * 
-	 * @param name
-	 * @return
-	 */
-	public List<MetaData> findForName(String name){
-		List<String> ids = findIDsForName(name);
-		return getAll(ids);
-	}
-	
-	
-	private List<String> findIDsForName(String name){
-		Vector<String> out = new Vector<String>();
-		Iterator<String> idIt = idMap.keySet().iterator();
-		while(idIt.hasNext() ){
-			String currID = idIt.next();
-			String currName = idMap.get(currID).getName();
-			if(currName.equals(name)){
+
+	private List<String> findForName(final String name) {
+		final Vector<String> out = new Vector<String>();
+		final Iterator<String> idIt = idMap.keySet().iterator();
+		while (idIt.hasNext()) {
+			final String currID = idIt.next();
+			final String currName = idMap.get(currID).getName();
+			if (currName.equals(name)) {
 				out.add(currID);
 			}
 		}
 		return out;
 	}
 	
+	private List<String> findAfter(Date after){
+		Vector<String> out = new Vector<String>();
+		Collection<Vector<String>> idLists = timeMap.tailMap(after, true).values();
+		for(Vector<String> ids : idLists){
+			out.addAll(ids);
+		}
+		return out;
+	}
+	
+	private List<String> findBefore(Date before){
+		Vector<String> out = new Vector<String>();
+		Collection<Vector<String>> idLists = timeMap.headMap(before, true).values();
+		for(Vector<String> ids : idLists){
+			out.addAll(ids);
+		}
+		return out;
+	}
+	
+	private List<String> findTextContains(String text){
+		List<String> names = findNameContains(text);
+		List<String> desc = findDescriptionContains(text);
+		names.retainAll(desc);
+		return names;
+	}
+	
+	private List<String> findNameContains(String snippet){
+		final Vector<String> out = new Vector<String>();
+		final Iterator<String> idIt = idMap.keySet().iterator();
+		while (idIt.hasNext()) {
+			final String currID = idIt.next();
+			final String currName = idMap.get(currID).getName();
+			if (currName.contains(snippet)) {
+				out.add(currID);
+			}
+		}
+		return out;
+	}
+	
+	private List<String> findDescriptionContains(String snippet){
+		final Vector<String> out = new Vector<String>();
+		final Iterator<String> idIt = idMap.keySet().iterator();
+		while (idIt.hasNext()) {
+			final String currID = idIt.next();
+			final String currDesc = idMap.get(currID).getDescription();
+			if (currDesc.contains(snippet)) {
+				out.add(currID);
+			}
+		}
+		return out;
+	}
+
 	/**
-	 * Currently returning list contains null entries when an non-existent id is given.
+	 * Currently returning list contains null entries when an non-existent id is
+	 * given.
+	 * 
 	 * @param ids
 	 * @return
 	 */
-	private List<MetaData> getAll(List<String> ids){
-		Vector<MetaData> out = new Vector<MetaData>();
-		if(ids != null && ids.size()>0){
-			for(String id : ids){
+	private List<MetaData> getAll(final List<String> ids) {
+		final Vector<MetaData> out = new Vector<MetaData>();
+		if (ids != null && ids.size() > 0) {
+			for (final String id : ids) {
 				out.add(idMap.get(id));
 			}
 		}
@@ -139,7 +230,7 @@ class MetaDataStorage {
 			return true;
 		} else {
 			final Vector<String> v = timeMap.get(d);
-			if (v != null) {
+			if (v != null && !v.contains(meta.getId() )) {
 				v.add(meta.getId());
 				return true;
 			}
