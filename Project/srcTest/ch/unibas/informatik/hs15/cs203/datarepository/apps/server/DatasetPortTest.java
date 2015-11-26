571d3ec8a93445785d7e01cf0eb3d4c3c93359c6
Silvan Heller <silvanheller@me.com>
2015-11-26 14:06:21 +0100

    Duplicated Utils Improved Test Added more logging to DatasetPort

--- Project/srcTest/ch/unibas/informatik/hs15/cs203/datarepository/apps/server/DatasetPortTest.java
+++ Project/srcTest/ch/unibas/informatik/hs15/cs203/datarepository/apps/server/DatasetPortTest.java
@@ -2,43 +2,60 @@ package ch.unibas.informatik.hs15.cs203.datarepository.apps.server;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import ch.unibas.informatik.hs15.cs203.datarepository.api.APITestCase;
 import ch.unibas.informatik.hs15.cs203.datarepository.api.CompletenessDetection;
-import ch.unibas.informatik.hs15.cs203.datarepository.processing.Factory;
+import ch.unibas.informatik.hs15.cs203.datarepository.Utils;
 
-public class DatasetPortTest extends APITestCase{
-	
+public class DatasetPortTest extends APITestCase {
+
 	DatasetPort port;
 	Path HTMLOverview;
 	CompletenessDetection strategy = new MockCompletenessDetection();
 	DatasetPortConfiguration config;
 	Path logFile;
+	Path incomingDir;
 
 	@Test
-	public void testStart() {
-		init();
-		port.start();
-	}
-	
-	@Test
-	public void testAdd(){
+	public void testStart() throws IOException, InterruptedException {
 		init();
 		(new Thread(new PortRunnable(port))).start();
+		Thread.sleep(1000);
 	}
-	
-	private void init(){
+
+	@Test
+	public void testAdd() throws IOException, InterruptedException {
+		init();
+		(new Thread(new PortRunnable(port))).start();
+		Thread.sleep(1000);
+		File file = new File(incomingDir.toFile(), "example.txt");
+	    String[] data = Utils.createExampleData(file, ":hello world!");
+	}
+
+	private void init() throws IOException {
+
+		incomingDir = new File("test-incoming-dir").toPath();
+		Utils.delete(incomingDir.toFile());
+		assertEquals(false, incomingDir.toFile().exists());
+		incomingDir.toFile().mkdirs();
+
 		HTMLOverview = Paths.get(repository.toString(), "table.html");
-		//TODO Add CompletenessDetection
+
 		logFile = Paths.get(repository.toString(), "server.log");
-		config = new DatasetPortConfiguration(workingDir.toPath(), HTMLOverview, logFile, 2, strategy.getClass());
-		port = DatasetPort.getDatasetPort(workingDir.toPath(), config, dataRepository);
+		// TODO Remove this line later
+		logFile.toFile().createNewFile();
+
+		config = new DatasetPortConfiguration(incomingDir, HTMLOverview,
+				logFile, 2, strategy.getClass());
+
+		port = DatasetPort.getDatasetPort(workingDir.toPath(), config,
+				dataRepository);
 	}
 
 }
@@ -48,12 +65,12 @@ public class DatasetPortTest extends APITestCase{
  */
 class PortRunnable implements Runnable {
 	DatasetPort port;
-	
-	PortRunnable(DatasetPort port){
+
+	PortRunnable(DatasetPort port) {
 		this.port = port;
 	}
-	
-    public void run() {
-        port.start();
-    }
+
+	public void run() {
+		port.start();
+	}
 }
