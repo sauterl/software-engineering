2078cd980704f6eb6327d2104eb18e2488658d5f
sauterl <loris.sauter@gmail.com>
2015-11-27 22:23:45 +0100

    Spiced the HTML overview up.\nTotal overkill again, but it looks nice and this is always a PLUS

--- Project/src/ch/unibas/informatik/hs15/cs203/datarepository/apps/server/OverviewWriter.java
+++ Project/src/ch/unibas/informatik/hs15/cs203/datarepository/apps/server/OverviewWriter.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 import ch.unibas.informatik.hs15.cs203.datarepository.api.MetaData;
@@ -12,6 +13,8 @@ import ch.unibas.informatik.hs15.cs203.datarepository.common.PrintUtils;
 
 class OverviewWriter {
 	private static final String LF = System.getProperty("line.separator");
+
+	private static final String META_CHARSET = "<meta charset=\"utf-8\"/>";
 
 	private static final String TABLE_HEADER = "<th>ID</th><th>Name</th><th>Timestamp</th><th>Number of Files</th><th>Size</th><th>Description</th>";
 
@@ -33,6 +36,8 @@ class OverviewWriter {
 
 	private static final String TABLE_CLOSE = "</table>";
 
+	private static final String DOCTYPE = "<!DOCTYPE html>";
+	
 	private static final String createBody(final String body) {
 		return wrapWithTag(body, BODY_OPEN, BODY_CLOSE);
 	}
@@ -64,6 +69,15 @@ class OverviewWriter {
 		return String.format(ROW_CONTENT_TEMPLATE, meta.getId(), meta.getName(),
 				PrintUtils.DATE_TIME_FORMAT.format(meta.getTimestamp()), meta.getNumberOfFiles(),
 				meta.getSize(), desc);
+	}
+	
+	private static final String createStyle(List<String> rules){
+		StringBuilder sb = new StringBuilder();
+		for(String s : rules){
+			sb.append(s);
+			sb.append(LF);
+		}
+		return wrapWithTag(sb.toString(), "<style>", "</style>");
 	}
 
 	private static final String createTable(final String tableBody) {
@@ -106,12 +120,22 @@ class OverviewWriter {
 		}
 		sb.insert(0, createRow(TABLE_HEADER));
 		String table = createTable(sb.toString() );
-		String style = wrapWithTag("body {font-family: sans-serif;}"+LF+"td {padding: 15px;}", "<style>", "</style>");
-		String head = createHead("<meta charset=\"utf-9\"/>"+LF+style);
-		String header = createHeader("data-repository contents overview");
+		String style = createStyle(getDefaultStyleRules() );
+		String head = createHead(META_CHARSET+LF+style);
+		String header = createHeader("Datasets Overview");
 		String body = createBody(header+LF+table);
 		String page = createPage(head+LF+body);
-		writeFile(page);
+		writeFile(DOCTYPE+LF+page);
+	}
+	
+	private List<String> getDefaultStyleRules(){
+		ArrayList<String> out = new ArrayList<String>();
+		out.add("body {font-family: sans-serif; padding: 10px;}");
+		out.add("table {border-collapse: collapse;}");
+		out.add("td,th {padding: 4px 8px 2px 8px;"+LF+"font-size: 1em;"+LF+"border: 1px solid black;}");
+		out.add("th {text-align: left;"+LF+"padding-top: 6px; padding-bottom: 4px;"+LF+"font-size: 1.1em;"+LF+"background-color: #BBBBBB}");
+		out.add("tr:nth-child(odd) td {background-color: #E4E4E4}");
+		return out;
 	}
 	
 	private void writeFile(String contents){
