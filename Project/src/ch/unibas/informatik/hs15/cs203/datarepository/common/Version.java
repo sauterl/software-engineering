c8ea710ab3f9c931445dc87062c0598ea9e60f67
Silvan Heller <silvanheller@me.com>
2015-12-09 16:02:14 +0100

    Replace was not deleting the created temp-files

--- Project/src/ch/unibas/informatik/hs15/cs203/datarepository/common/Version.java
+++ Project/src/ch/unibas/informatik/hs15/cs203/datarepository/common/Version.java
@@ -2,6 +2,6 @@ package ch.unibas.informatik.hs15.cs203.datarepository.common;
 
 public class Version {
 
-	public static final String VERSION = "dev-build 0.2";
+	public static final String VERSION = "dev-build 0.3.1";
 
 }
